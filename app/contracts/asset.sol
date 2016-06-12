c2a14af296f7be8fc394953b7abfa8c3919adf20
Martin Delille <martin@phonations.com>
2016-06-12 09:59:53 +0200

    string => bytes16

--- app/contracts/asset.sol
+++ app/contracts/asset.sol
@@ -1,12 +1,12 @@
 contract Database {
-    mapping (string => address) public database;
+    mapping (bytes16 => address) public database;
 
     function Database() {
     }
 
-    function newAsset(string _isrc,
-                      string _iswc,
-                      string _ipi,
+    function newAsset(bytes16 _isrc,
+                      bytes16 _iswc,
+                      bytes16 _ipi,
                       address _sacem) public {
         Asset _contract = new Asset(_isrc, _iswc, _ipi, _sacem);
         database[_isrc] = _contract;
@@ -14,12 +14,12 @@ contract Database {
 }
 
 contract Asset {
-    string public isrc;
-    string public iswc;
-    string public ipi;
+    bytes16 public isrc;
+    bytes16 public iswc;
+    bytes16 public ipi;
     address public sacem;
 
-    function Asset(string _isrc, string _iswc, string _ipi, address _sacem) {
+    function Asset(bytes16 _isrc, bytes16 _iswc, bytes16 _ipi, address _sacem) {
         isrc = _isrc;
         iswc = _iswc;
         ipi = _ipi;
